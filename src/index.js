951df67ddfc3430ecc217ae0ce21b2982d24ec79
Teja Dandu <teja.dandu484@gmail.com>
2023-01-25 22:33:36 +0530

    Added v1 for tweet model and hashtag model

--- src/index.js
+++ src/index.js
@@ -12,14 +12,5 @@ app.listen(3000, async () =>{
     console.log('Server started');
     await connect();
     console.log('Mongodb connected');
-    // const tweet = await Tweet.create({
-    //     content: 'Fourth tweet',
 
-    // });
-    // const tweets = await Tweet.find({userEmail: 'a@b.com'});
-    const tweetRepo = new TweetRepository();
-    // const tweet = await tweetRepo.getAll(2, 4);
-    // console.log(tweet[1].contentWithEmail);
-    const tweet = await tweetRepo.create({content: 'With hooks'});
-    console.log(tweet);
 });
--- src/models/tweet.js
+++ src/models/tweet.js
@@ -4,27 +4,18 @@ const tweetSchema = new mongoose.Schema({
     content: {
         type: String,
         required: true,
+        max: [250, 'Tweet cannot be more than 250 chars']
 
     },
-    userEmail: {
-        type: String
-    },
-    comments: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Comment'
-            }
-        ]
+    hashtags: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Hashtag'
+        }
+    ]
+
 }, { timestamps: true});
 
-tweetSchema.virtual('contentWithEmail').get(function process(){
-    return `${this.content} \n Created By ${this.userEmail}`;
-});
 
-tweetSchema.pre('save', function(next){
-    console.log('Inside hook');
-    this.content = this.content + '....';
-    next();
-});
 const Tweet = mongoose.model('Tweet', tweetSchema);
 module.exports = Tweet;
