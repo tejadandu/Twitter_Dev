e75907a7ad1754f8578b587d540d6d86887383d1
Teja Dandu <teja.dandu484@gmail.com>
2023-02-01 11:50:41 +0530

    Added tweet service with hashtag processing

--- src/index.js
+++ src/index.js
@@ -4,18 +4,11 @@ const connect = require('./config/database');
 
 const app = express();
 
-const TweetRepository = require('./repository/tweet-repository');
-const Comment = require('./models/comment');
-const Tweet = require('./models/tweet');
-
 app.listen(3000, async () =>{
 
     console.log('Server started');
     await connect();
     console.log('Mongodb connected');
 
-    const tweets = await Tweet.find({
-        content: ['1234567']
-    });
-    console.log(tweets);
+
 });
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -1,16 +1,33 @@
-const { TweetRepository } = require('../repository/index');
+const { TweetRepository, HashtagRepository } = require('../repository/index');
 
 class TweetService {
     constructor(){
         this.tweetRepository = new TweetRepository();
+        this.hashtagRepository = new HashtagRepository();
     }
 
     async create(data){
         const content = data.content;
-        const tags = content.match(/#[a-zA-Z0-9_]+/g); // this regex extracts hashtags
-        tags = tags.map((tag) => tag.susbstring(1));
+        let tags = content.match(/#[a-zA-Z0-9_]+/g)
+        tags = tags.map((tag) => tag.substring(1)); // this regex extracts hashtags
         console.log(tags);
         const tweet = await this.tweetRepository.create(data);
+
+        let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
+        // alreadyPresentTags = alreadyPresentTags.map((tag) => tag.title);
+        console.log(alreadyPresentTags);
+        let titleOfPresentTags = tags.filter(tag => !alreadyPresentTags.includes(tag));
+        let newTags = titleOfPresentTags.map(tag => {
+            return {
+                title:tag, tweets:[tweet.id]
+            }
+        });
+        await this.hashtagRepository.bulkCreate(newTags);
+
+        alreadyPresentTags.forEach((tag) => {
+            tag.tweets.push(tweet.id);
+            tag.save();
+        });
         // todo create hashtags and added here
         /*
         * 1. bulkcreate in mongoose
